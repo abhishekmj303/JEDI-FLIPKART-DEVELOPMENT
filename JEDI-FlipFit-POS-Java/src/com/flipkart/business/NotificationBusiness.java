42f44ebd986dcd55ba53bb260ede99e2f8f62f9e
shrish.kumar <shrish.kumar@fk1089-232944l.fkipl.flipkart.com>
2025-02-12 15:57:59 +0530

    Business Owner and notification

--- JEDI-FlipFit-POS-Java/src/com/flipkart/business/NotificationBusiness.java
+++ JEDI-FlipFit-POS-Java/src/com/flipkart/business/NotificationBusiness.java
@@ -1,5 +1,20 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.Notification;
+
 public class NotificationBusiness {
-    
+
+    public void sendNotification(Notification notification) {
+        System.out.println("Notification sent to user ID " + notification.getUserId());
+        System.out.println("Message: " + notification.getMessage());
+        System.out.println("Date: " + notification.getDateTime());
+    }
+
+    public void markAsRead(int notificationId) {
+        System.out.println("Notification ID " + notificationId + " marked as read.");
+    }
+
+    public void listAllNotificationsForUser(int userId) {
+        System.out.println("Listing notifications for user with ID: " + userId);
+    }
 }
