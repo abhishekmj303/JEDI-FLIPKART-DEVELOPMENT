5b3795e19cb8d57a88fc0af909af5180fdd8dac4
Abhishek M J <abhishekmj303@gmail.com>
2025-02-14 11:10:30 +0530

    update admin

--- JEDI-FlipFit-POS-Java/src/com/flipkart/business/FlipFitGymAdminBusiness.java
+++ JEDI-FlipFit-POS-Java/src/com/flipkart/business/FlipFitGymAdminBusiness.java
@@ -12,28 +12,15 @@ import com.flipkart.bean.FlipFitGymCenter;
  */
 public class FlipFitGymAdminBusiness implements FlipFitGymAdminInterface {
 	private HashMap<Integer, FlipFitGymOwner> owners;
-	private int ownerId;
 	private HashMap<Integer, FlipFitGymCenter> centres;
-	private int centerId;
-
-	
-
 
 	 public FlipFitGymAdminBusiness(
 		        HashMap<Integer, FlipFitGymOwner> owners,
 		        HashMap<Integer, FlipFitGymCenter> centres
-		    ) {
-		        this.owners = owners;
-		        this.centres = centres;
-		        this.ownerId = 1;
-		        this.centerId = 1;
-		    }
-
-	
-	public FlipFitGymAdminBusiness() {
-		// TODO Auto-generated constructor stub
+		) {
+			this.owners = owners;
+			this.centres = centres;
 	}
-
 
 	public void addGymAdmin(int userId) {
 		System.out.println("Adding gym admin ID: " + userId);
@@ -41,8 +28,9 @@ public class FlipFitGymAdminBusiness implements FlipFitGymAdminInterface {
 	
 	public void approveGymOwner(int ownerId) {
 		if(owners.containsKey(ownerId)) {
-		System.out.println("Gym owner approved with ID: " + ownerId);
-		
+			FlipFitGymOwner owner = owners.get(ownerId);
+			owner.setApproved(true);
+			System.out.println("Gym owner approved with ID: " + ownerId);
 	}else {
 		System.out.println("Gym owner ID not found: " + ownerId);
 	}
@@ -50,6 +38,8 @@ public class FlipFitGymAdminBusiness implements FlipFitGymAdminInterface {
 	
 	public void approveGymCentre(int centreId) {
 		if(centres.containsKey(centerId)) {
+			FlipFitGymCenter center = centers.get(ownerId);
+			center.setApproved(true);
 			System.out.println("Gym center approved with ID: " + centreId);
 		}else {
 		System.out.println("Gym centre ID not found: " + centreId);
