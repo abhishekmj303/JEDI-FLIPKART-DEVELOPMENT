b4716237d6ec1963c1979b2f5ca110cbabae21d1
ayush.kumargupta <ayush.kumargupta@flipkart.com>
2025-02-15 14:22:40 +0530

    Updated FlipFitUser Business, Dao

--- JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitUserDao.java
+++ JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitUserDao.java
@@ -3,9 +3,23 @@
  */
 package com.flipkart.dao;
 
-/**
- * 
- */
+import com.flipkart.bean.FlipFitUser;
+
 public interface FlipFitUserDao {
 
+    int addUser(FlipFitUser user);
+    
+    boolean updateUser(String email, String name, int roleId);
+    
+    boolean updatePassword(String email, String oldPassword, String newPassword);
+    
+    FlipFitUser getUserByEmail(String email);
+    
+    void listAllUsers();
+    
+    boolean login(String email, String password);
+    
+    String generateToken(int userId);
+    
+    boolean validateToken(String token);
 }
