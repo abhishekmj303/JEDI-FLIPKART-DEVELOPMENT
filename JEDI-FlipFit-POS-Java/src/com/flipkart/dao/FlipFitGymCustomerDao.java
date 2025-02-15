a61d2deab90b36e2507fa96e909a2e86302fffb9
ayush.kumargupta <ayush.kumargupta@flipkart.com>
2025-02-15 12:51:21 +0530

    Updated FlipFitGymCustomer Business and DAO

--- JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitGymCustomerDao.java
+++ JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitGymCustomerDao.java
@@ -8,4 +8,25 @@ package com.flipkart.dao;
  */
 public interface FlipFitGymCustomerDao {
 
+    void addGymCustomer(FlipFitGymCustomer customer);
+
+    void setPreferredCity(int userId, String city);
+
+    void bookSlot(FlipFitSlotBooking booking);
+
+    boolean cancelBooking(int bookingId);
+
+    void listAllCentersByCity(String city);
+
+    void viewAvailableSlots(int gymCenterId);
+
+    void viewBookedSlots(int userId);
+
+    void processPayment(FlipFitPayment payment);
+
+    boolean refundPayment(int paymentId);
+
+    String getPaymentStatus(int paymentId);
+
+    void sendNotification(FlipFitNotification notification);
 }
