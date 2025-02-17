95aed58854698527c5c558558da665964fde2c64
Abhishek M J <abhishekmj303@gmail.com>
2025-02-17 15:21:02 +0530

    Remove docs

--- JEDI-FlipFit-POS-Java/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.flipkart.bean"},{"l":"com.flipkart.business"},{"l":"com.flipkart.client"},{"l":"com.flipkart.constant"},{"l":"com.flipkart.dao"},{"l":"com.flipkart.datasource"},{"l":"com.flipkart.exception"},{"l":"com.flipkart.helper"},{"l":"com.flipkart.utils"}];updateSearchResults();
