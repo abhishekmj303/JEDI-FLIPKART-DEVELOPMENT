4d253b3894cefafe2c38e8453b6f7e9d3cc847e0
shrish.kumar <shrish.kumar@fk1089-232944l.fkipl.flipkart.com>
2025-02-17 14:38:20 +0530

    Added Javadoc

--- JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitGymAdminDao.java
+++ JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitGymAdminDao.java
@@ -1,16 +1,30 @@
-/**
- * 
- */
 package com.flipkart.dao;
 
 /**
- * 
+ * Interface for managing gym admin data operations.
+ * This interface defines methods for approving gym owners and centers,
+ * and listing all gym centers.
  */
 public interface FlipFitGymAdminDao {
-	
-	public boolean approveGymOwner(int ownerId);
-	
-	public boolean approveGymCentre(int centreId);
-	
-	public void listAllGymCentres();
+
+    /**
+     * Approves a gym owner.
+     * 
+     * @param ownerId The ID of the gym owner to be approved.
+     * @return true if the gym owner was successfully approved, false otherwise.
+     */
+    public boolean approveGymOwner(int ownerId);
+
+    /**
+     * Approves a gym center.
+     * 
+     * @param centreId The ID of the gym center to be approved.
+     * @return true if the gym center was successfully approved, false otherwise.
+     */
+    public boolean approveGymCentre(int centreId);
+
+    /**
+     * Lists all the gym centers in the system.
+     */
+    public void listAllGymCentres();
 }
--- JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitGymOwnerDao.java
+++ JEDI-FlipFit-POS-Java/src/com/flipkart/dao/FlipFitGymOwnerDao.java
@@ -2,10 +2,51 @@ package com.flipkart.dao;
 
 import java.util.List;
 
+/**
+ * Interface for managing gym owner data operations.
+ * This interface defines methods for adding gym owners, adding gym centers and slots,
+ * and viewing the status of gym slots.
+ */
 public interface FlipFitGymOwnerDao {
+
+    /**
+     * Adds a new gym owner to the system.
+     * 
+     * @param userId The ID of the gym owner.
+     * @param aadhaarNo The Aadhaar number of the gym owner.
+     * @param pan The PAN number of the gym owner.
+     * @param phoneNo The phone number of the gym owner.
+     */
     public void addGymOwnerDAO(int userId, String aadhaarNo, String pan, String phoneNo);
+
+    /**
+     * Adds a new gym center for the gym owner.
+     * 
+     * @param ownerId The ID of the gym owner adding the center.
+     * @param centreName The name of the gym center.
+     * @param city The city where the gym center is located.
+     * @param address The address of the gym center.
+     * @param seatsPerHour The number of available seats per hour at the gym center.
+     * @param startTimeMorning The start time of the morning session.
+     * @param endTimeMorning The end time of the morning session.
+     * @param startTimeEvening The start time of the evening session.
+     * @param endTimeEvening The end time of the evening session.
+     * @return The ID of the newly added gym center.
+     */
     public int addCenterDAO(int ownerId, String centreName, String city, String address, int seatsPerHour, 
             String startTimeMorning, String endTimeMorning, String startTimeEvening, String endTimeEvening);
+
+    /**
+     * Adds available time slots to a gym center.
+     * 
+     * @param centerId The ID of the gym center for which the slots are being added.
+     * @param startTimes A list of start times for the available slots.
+     * @param endTimes A list of end times for the available slots.
+     */
     public void addSlot(int centerId, List<String> startTimes, List<String> endTimes);
+
+    /**
+     * Views the status of the slots at all gym centers.
+     */
     public void viewSlotsStatusDAO();
 }
